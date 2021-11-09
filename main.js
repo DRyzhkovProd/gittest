1eba44e3460a88bfa98ebd09d570218d4a2d0d59
DRyzhkovProd <dryzhkov98@yandex.ru>
2021-11-09 22:41:43 +0300

    write print func in main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+const print =(value) => {
+    return console.log(value)
+}
+
+print('test') // this function emulate print by python
--- test.md
+++ test.md
@@ -1,3 +1,4 @@
 **LOREM IPSUM** <br>
 _test merge !_
 `console.log("test")`
+_some test_
