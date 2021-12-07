bd2514a56786cb0d45616e3061b3a1b34ea2b039
dryzhkov <dryzhkov98@yandex.ru>
2021-12-07 13:09:40 +0300

    remove client from repository

---
+++ client/mine.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", ()=> {
+  document.body.style.background = "red"
+
+  setTimeout(()=> {
+    document.body.style.background = "blue"
+
+}, 3000)
+  console.log('hello')
+})
